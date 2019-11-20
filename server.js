818791ef06c4410b987083f79150e407e9eef6d0
tawny francis <tawnyfrancis@gmail.com>
2019-11-20 11:09:14 -0500

    copied code into repo on my computer- worked collab on justins computer

---
+++ server.js
@@ -0,0 +1,104 @@
+// Dependencies
+// =============================================================
+var express = require("express");
+var path = require("path");
+
+// Sets up the Express App
+// =============================================================
+var app = express();
+var PORT = 3000;
+
+// Sets up the Express app to handle data parsing
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+// Star Wars Characters (DATA)
+// =============================================================
+var tables = [
+  {
+    id: "1",
+    name: "Justin",
+    email: "email@email.com",
+    phone: "123456789"
+  }
+];
+
+var waitlist = [
+    {
+        id: "6",
+        name: "Bob",
+        email: "sprinkles@gmail.com",
+        phone: "234567890"
+    }
+]
+// Routes
+// =============================================================
+
+// Basic route that sends the user first to the AJAX Page
+app.get("/", function(req, res) {
+  res.sendFile(path.join(__dirname, "view.html"));
+});
+
+app.get("/add", function(req, res) {
+  res.sendFile(path.join(__dirname, "add.html"));
+});
+
+// Displays all characters
+app.get("/tables", function(req, res) {
+    res.sendFile(path.join(__dirname, "tables.html"));
+});
+
+app.get("/api/tables", function(req, res) {
+    return res.json(tables);
+  });
+
+  app.get("/api/waitlist", function(req, res) {
+    return res.json(waitlist);
+  });
+
+
+// Displays a single character, or returns false
+app.get("/api/tables/:table", function(req, res) {
+  var chosen = req.params.table;
+
+  console.log(chosen);
+
+  for (var i = 0; i < tables.length; i++) {
+    if (chosen.toLowerCase() === tables[i].name.toLowerCase()) {
+      return res.json(tables[i]);
+    }
+  }
+
+  return res.json(false);
+});
+
+// Create New Characters - takes in JSON input
+app.post("/api/tables", function(req, res) {
+  // req.body hosts is equal to the JSON post sent from the user
+  // This works because of our body parsing middleware
+  var newTable = req.body;
+
+  // Using a RegEx Pattern to remove spaces from newCharacter
+  // You can read more about RegEx Patterns later https://www.regexbuddy.com/regex.html
+//   newTable.routeName = newTable.name.replace(/\s+/g, "").toLowerCase();
+
+console.log(newTable);
+
+if (tables.length > 4) {
+
+    waitlist.push(newTable);
+
+    res.json(newTable);
+} else {
+    tables.push(newTable);
+
+    res.json(newTable);
+}
+
+});
+
+// Starts the server to begin listening
+// =============================================================
+app.listen(PORT, function() {
+  console.log("App listening on PORT " + PORT);
+});
